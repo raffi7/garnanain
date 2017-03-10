1cca8c57d4f89fa93109dbe71bdeffeefed1c137
Samim Pezeshki <p.samim@gmail.com>
2017-03-10 14:44:37 +0330

    Shuffle questions

--- src/S1/index.js
+++ src/S1/index.js
@@ -1,33 +1,17 @@
 import React from 'react';
 import {
-  Button,
-  CardText,
-  Card,
-  CardTitle,
-  CardMenu,
-  CardActions,
   IconButton,
   Grid,
-  Icon,
-  FABButton,
   Cell,
-  ProgressBar,
-  IconToggle,
-  Tooltip,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions
 } from 'react-mdl';
+import { shuffle } from 'underscore';
 import Layout from '../../components/Layout';
 import ResultDialog from '../../components/ResultDialog';
 import s from '../Questionstyles.css';
-import Link from '../../components/Link';
 import AnswerCard from '../../components/AnswerCard';
 import history from '../history';
 import Timer from '../../components/Timer';
-import Questions from './questions.js';
-
+import Questions from './questions';
 
 export default class Question extends React.Component {
   constructor(props) {
@@ -38,21 +22,10 @@ export default class Question extends React.Component {
     };
   }
 
-  componentDidMount() {
-    document.title = 'Գ1';
-  }
-
-  correct = () => {
+  answer = (correct) => {
     this.setState({
-      openDialogCorrect: true,
-      openDialogWrong: false,
-    });
-  }
-
-  wrong = () => {
-    this.setState({
-      openDialogWrong: true,
-      openDialogCorrect: false,
+      openDialogCorrect: correct,
+      openDialogWrong: !correct,
     });
   }
 
@@ -60,32 +33,42 @@ export default class Question extends React.Component {
     history.push({ pathname: '/science' });
   }
 
-  render() {
-    let answerColor = '#0d47a1';
-    let x = 0;
-    if (x === 1) {
-      answerColor = 'green';
-    }
-
+  componentWillMount() {
     const id = parseInt(this.props.route.params.id, 10);
-    const question = Questions[id];
+    this.question = Questions[id];
+    this.answers = shuffle(this.question.answers);
+  }
 
+  componentDidMount() {
+    document.title = 'Գ1';
+  }
+
+  render() {
     return (
       <Layout className={s.content}>
-        <h1 className={s.fontstyle1} style={{marginTop: '20px'}}>
-          <Grid className="demo-grid-1" style={{marginTop: '20px'}}>
+        <h1 className={s.fontstyle1} style={{ marginTop: '20px' }}>
+          <Grid className="demo-grid-1" style={{ marginTop: '20px' }}>
             <Cell col={1}>
-              <IconButton name="arrow_back" colored onClick={this.gotoScience}/>
+              <IconButton name="arrow_back" colored onClick={this.gotoScience} />
             </Cell>
-            <Cell col={11} className={s.ScienceQuestionFont}>{question.text}</Cell>
+            <Cell col={11} className={s.ScienceQuestionFont}>{this.question.text}</Cell>
           </Grid>
         </h1>
 
-        <Grid style={{marginLeft: '-10px'}} className="demo-grid-1">
-          <Cell col={3}><AnswerCard color={'#0d47a1'} text={question.answers[0]} onClick={this.correct} fontSize={question.fontSize} /></Cell>
-          <Cell col={3}><AnswerCard color={'#0d47a1'} text={question.answers[1]} onClick={this.wrong} /></Cell>
-          <Cell col={3}><AnswerCard color={'#0d47a1'} text={question.answers[2]} onClick={this.wrong} /></Cell>
-          <Cell col={3}><AnswerCard color={'#0d47a1'} text={question.answers[3]} onClick={this.wrong} /></Cell>
+        <Grid style={{ marginLeft: '-10px' }} className="demo-grid-1">
+
+          {this.answers.map((answer, i) =>
+            (
+              <Cell col={3} key={i}>
+                <AnswerCard
+                  color={'#0d47a1'}
+                  text={answer.text}
+                  onClick={() => this.answer(answer.correct)}
+                  fontSize={answer.fontSize}
+                />
+              </Cell>
+            ))}
+
         </Grid>
         <div style={{ marginLeft: '299px', marginTop: '15px' }}> <Timer timeout={5} /></div>
         <ResultDialog score="5" correct={this.state.openDialogCorrect} wrong={this.state.openDialogWrong} />
--- src/S1/questions.js
+++ src/S1/questions.js
@@ -1,36 +1,40 @@
 const Questions = [
   {
-    id: 0,
     text: 'Որ Երաժիշտ և երքահանն էր խլականջ',
-    correct: 'correct answer',
-    wrong: [
-      'first wrong',
-      'second wrong',
-      'third wrong',
-    ],
-  },
-  {
-    id: 1,
-    text: 'Second question',
-    correct: 'correct answer',
-    wrong: [
-      'first wrong',
-      'second wrong',
-      'third wrong',
-    ],
-  },
-  {
-    id: 2,
-    fontSize: '15px',
-    text: 'harx 3',
     answers: [
-      'first answer',
-      'second answer',
-      'third answer',
-      'fourth answer',
+      {
+        text: 'Answer 1',
+        correct: true,
+      }, {
+        text: 'Answer 2',
+        correct: false,
+      }, {
+        text: 'Answer 3',
+        correct: false,
+      }, {
+        text: 'Answer 4',
+        correct: false,
+      },
     ],
   },
-
+  {
+    text: 'Q2',
+    answers: [
+      {
+        text: 'Answer 1',
+        correct: true,
+      }, {
+        text: 'Answer 2',
+        correct: false,
+      }, {
+        text: 'Answer 3',
+        correct: false,
+      }, {
+        text: 'Answer 4',
+        correct: false,
+      },
+    ],
+  },
 ];
 
 export default Questions;
