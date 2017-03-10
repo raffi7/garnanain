156029f5fb8031527b6b79a1cdcf49c20219c387
Samim Pezeshki <p.samim@gmail.com>
2017-03-10 15:01:55 +0330

    Add questions for music

---
+++ src/M1/questions.js
@@ -0,0 +1,16 @@
+/* eslint-disable global-require */
+
+const Questions = [
+  {
+    name: 'name',
+    lastName: 'last name',
+    src: require('./files/m.ogg'),
+  },
+  {
+    name: 'name 2',
+    lastName: 'last name 2',
+    src: require('./files/m.ogg'),
+  },
+];
+
+export default Questions;
