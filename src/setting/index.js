9cb6e5a4f9b2eb723ce34555f012f466ad7f2c31
Samim Pezeshki <p.samim@gmail.com>
2017-03-10 16:08:56 +0330

    Add teams

--- src/setting/index.js
+++ src/setting/index.js
@@ -1,64 +1,51 @@
 import React from 'react';
 import {
-  Button,
-  CardText,
-  Card,
-  CardTitle,
-  CardMenu,
-  CardActions,
   IconButton,
+  List,
+  ListItem,
   Grid,
   Icon,
   FABButton,
   Cell,
-  ProgressBar,
-  IconToggle,
-  Tooltip,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  List,
-  ListItem
 } from 'react-mdl';
 import db from 'localforage';
 import Layout from '../../components/Layout';
-import ResultDialog from '../../components/ResultDialog';
 import s from './styles.css';
-import Link from '../../components/Link';
-import AnswerCard from '../../components/AnswerCard';
 import history from '../history';
+import teams from '../teams';
 
-export default class Question extends React.Component {
+export default class Settings extends React.Component {
 
   componentDidMount() {
     document.title = 'Setting';
     setInterval(this.addCounter, 1000);
   }
+
   clearDb = () => {
-    db.setItem('science', {})
-    .catch((err) => {
-      console.log(err);
-    });
-    db.setItem('images', {})
-    .catch((err) => {
-      console.log(err);
-    });
-    db.setItem('musics', {})
-    .catch((err) => {
-      console.log(err);
-    });
-    db.setItem('sport', {})
-    .catch((err) => {
-      console.log(err);
-    });
+    db.setItem('science', {}).catch(console.log);
+    db.setItem('images', {}).catch(console.log);
+    db.setItem('musics', {}).catch(console.log);
+    db.setItem('sport', {}).catch(console.log);
   }
+
   clearScoreDb = () => {
-    db.setItem('scores', {})
-    .catch((err) => {
-      console.log(err);
-    });
+    db.setItem('scores', {}).catch(console.log);
   }
+
+  inc = (team) => {
+    db.getItem('scores').then((scores) => {
+      scores[team.id] = (scores[team.id] || 0) + 5;
+      db.setItem('scores', scores);
+    }).catch(console.log);
+  }
+
+  dec = (team) => {
+    db.getItem('scores').then((scores) => {
+      scores[team.id] = (scores[team.id] || 0) - 5;
+      db.setItem('scores', scores);
+    }).catch(console.log);
+  }
+
   gotoHome() {
     history.push({pathname: '/'}); // go to page function
   }
@@ -66,119 +53,53 @@ export default class Question extends React.Component {
   render() {
     return (
       <Layout className={s.content}>
-        <h1 className={s.fontstyle1} style={{
-          marginTop: '45px'
-        }}>
-        <Grid className="demo-grid-1" style={{
-          marginTop: '10px'
-        }}>
-          <Cell col={1}>
-            <IconButton name="home" colored onClick={this.gotoHome}/>
+        <h1 className={s.fontstyle1} style={{ marginTop: '45px' }}>
+          <Grid className="demo-grid-1" style={{ marginTop: '10px' }}>
+            <Cell col={1}>
+              <IconButton name="home" colored onClick={this.gotoHome} />
+            </Cell>
+            <Cell col={1}>Setting</Cell>
+          </Grid>
+        </h1>
+        <Grid className="demo-grid-2" style={{ marginTop: '-45px' }}>
+          <Cell col={6}>
+            <List>
+              <ListItem><span style={{ fontSize: '25px' }}>Score Manual Setting</span></ListItem>
+              {teams.map(team => (
+                 <ListItem key={team.id}>
+                   <span style={{ marginRight: '20px' }}>
+                     <FABButton onClick={() => this.dec(team)} colored style={{ color: '#eceff1', backgroundColor: '#f44336' }} mini>
+                       -5 </FABButton>
+                     <FABButton onClick={() => this.inc(team)} colored mini style={{ color: '#eceff1', backgroundColor: '#388e3c', marginLeft: '5px' }} >
+                       +5 </FABButton>
+                   </span>
+                   {team.name}
+                 </ListItem>
+               ))}
+            </List>
           </Cell>
-          <Cell col={1}>Setting</Cell>
-            </Grid>
-       </h1>
-       <Grid className="demo-grid-2" style={{
-         marginTop: '-45px'
-       }}>
-         <Cell col={6}>
-           <List>
-             <ListItem><span style={{fontSize: '25px'}}>Score Manual Setting</span></ListItem>
-             <ListItem>
-               <span style={{marginRight: '20px'}}>
-                 <FABButton colored style={{color: '#eceff1', backgroundColor: '#f44336'}} mini>
-                   -5
-                 </FABButton>
-                 <FABButton colored
-                   style={{color: '#eceff1',
-                           backgroundColor: '#388e3c',
-                           marginLeft: '5px'}} mini>
-                   +5
-                 </FABButton>
-              </span>
-               Team1
+          <Cell col={6}>
+            <List>
+              <ListItem><span style={{ fontSize: '25px' }}>General Setting</span></ListItem>
+              <ListItem>
+                <span style={{ marginRight: '20px' }}>
+                  <FABButton onClick={this.clearDb} colored style={{ color: '#37474f', backgroundColor: '#eceff1' }} mini>
+                    <Icon name="delete" />
+                  </FABButton>
+                </span>
+                Clear Question Arrangement
               </ListItem>
               <ListItem>
-                <span style={{marginRight: '20px'}}>
-                  <FABButton colored style={{color: '#eceff1', backgroundColor: '#f44336'}} mini>
-                    -5
+                <span style={{ marginRight: '20px' }}>
+                  <FABButton onClick={this.clearScoreDb} colored style={{ color: '#37474f', backgroundColor: '#eceff1' }} mini>
+                    <Icon name="delete" />
                   </FABButton>
-                  <FABButton colored
-                    style={{color: '#eceff1',
-                            backgroundColor: '#388e3c',
-                            marginLeft: '5px'}} mini>
-                    +5
-                  </FABButton>
-               </span>
-                 Team2
-               </ListItem>
-               <ListItem>
-                 <span style={{marginRight: '20px'}}>
-                   <FABButton colored style={{color: '#eceff1', backgroundColor: '#f44336'}} mini>
-                     -5
-                   </FABButton>
-                   <FABButton colored
-                     style={{color: '#eceff1',
-                             backgroundColor: '#388e3c',
-                             marginLeft: '5px'}} mini>
-                     +5
-                   </FABButton>
                 </span>
-                  Team3
-                </ListItem>
-                <ListItem>
-                  <span style={{marginRight: '20px'}}>
-                    <FABButton colored style={{color: '#eceff1', backgroundColor: '#f44336'}} mini>
-                      -5
-                    </FABButton>
-                    <FABButton colored
-                      style={{color: '#eceff1',
-                              backgroundColor: '#388e3c',
-                              marginLeft: '5px'}} mini>
-                      +5
-                    </FABButton>
-                 </span>
-                   Team4
-                 </ListItem>
-                 <ListItem>
-                   <span style={{marginRight: '20px'}}>
-                     <FABButton colored style={{color: '#eceff1', backgroundColor: '#f44336'}} mini>
-                       -5
-                     </FABButton>
-                     <FABButton colored
-                       style={{color: '#eceff1',
-                               backgroundColor: '#388e3c',
-                               marginLeft: '5px'}} mini>
-                       +5
-                     </FABButton>
-                  </span>
-                    Team5
-                  </ListItem>
-          </List>
-         </Cell>
-         <Cell col={6}>
-       <List>
-         <ListItem><span style={{fontSize: '25px'}}>General Setting</span></ListItem>
-         <ListItem>
-           <span style={{marginRight: '20px'}}>
-             <FABButton onClick={this.clearDb} colored style={{color: '#37474f', backgroundColor: '#eceff1'}} mini>
-               <Icon name="delete" />
-             </FABButton>
-          </span>
-            Clear Question Arrangement
-          </ListItem>
-        <ListItem>
-          <span style={{ marginRight: '20px' }}>
-            <FABButton onClick={this.clearScoreDb} colored style={{ color: '#37474f', backgroundColor: '#eceff1' }} mini>
-              <Icon name="delete" />
-              </FABButton>
-            </span>
-            Clear Score Arrangement
-          </ListItem>
-      </List>
-      </Cell>
-</Grid>
-
+                Clear Score Arrangement
+              </ListItem>
+            </List>
+          </Cell>
+        </Grid>
       </Layout>
     );
   }
---
+++ src/teams.js
@@ -0,0 +1,22 @@
+const Teams = [
+  {
+    id: 1,
+    name: 'teamA',
+    color: '#f8bbd0',
+    backColor: '#d81b60',
+  },
+  {
+    id: 2,
+    name: 'teamB',
+    color: '#f8bbd0',
+    backColor: '#d81b60',
+  },
+  {
+    id: 3,
+    name: 'teamC',
+    color: '#f8bbd0',
+    backColor: '#d81b60',
+  },
+];
+
+export default Teams;
