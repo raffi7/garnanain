d3689b22f0688fc236a2a0b62593147bbbcc8e26
Samim Pezeshki <p.samim@gmail.com>
2017-03-10 15:24:29 +0330

    Add questions to images

---
+++ src/P1/questions.js
@@ -0,0 +1,18 @@
+/* eslint-disable global-require */
+
+const Questions = [
+  {
+    name: 'name',
+    lastName: 'last name',
+    answerPic: require('./files/a.jpg'),
+    questionPic: require('./files/p.jpg'),
+  },
+  {
+    name: 'name2',
+    lastName: 'last name2',
+    answerPic: require('./files/a.jpg'),
+    questionPic: require('./files/p.jpg'),
+  },
+];
+
+export default Questions;
