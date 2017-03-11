9a0b8213ff018afd0d83d300e2c8409691a2855e
Raffi Dilanchian <raffidilanchian@gmail.com>
2017-03-11 14:45:00 +0330

    AnswerCard number update UI

--- src/SP1/index.js
+++ src/SP1/index.js
@@ -71,11 +71,11 @@ export default class Question extends React.Component {
             ))}
 
         </Grid>
-        <Grid style={{ fontSize: '30px'  }} className="demo-grid-1">
-          <Cell offset={1} col={1} align="center"><Icon style={{ color: '#283593', fontSize: '50px' }} name="looks_one" /></Cell>
-          <Cell offset={2} col={1} ><Icon style={{ color: '#283593', fontSize: '50px' }} name="looks_two" /></Cell>
-          <Cell offset={2} col={1}><Icon style={{ color: '#283593', fontSize: '50px' }} name="looks_3" /></Cell>
-          <Cell offset={2} col={1}><Icon style={{ color: '#283593', fontSize: '50px' }} name="looks_4" /></Cell>
+        <Grid style={{ fontSize: '30px', marginTop: '-50px'  }} className="demo-grid-1">
+          <Cell offset={1} col={1} align="center"><Icon style={{ color: '#283593', fontSize: '50px', marginLeft: '90px' }} name="looks_one" /></Cell>
+          <Cell offset={2} col={1} ><Icon style={{ color: '#283593', fontSize: '50px', marginLeft: '90px' }} name="looks_two" /></Cell>
+          <Cell offset={2} col={1}><Icon style={{ color: '#283593', fontSize: '50px', marginLeft: '90px' }} name="looks_3" /></Cell>
+          <Cell offset={2} col={1}><Icon style={{ color: '#283593', fontSize: '50px', marginLeft: '90px' }} name="looks_4" /></Cell>
         </Grid>
         <div style={{ marginLeft: '299px', marginTop: '15px' }}> <Timer timeout={20} /></div>
         <ResultDialog score="5" correct={this.state.openDialogCorrect} wrong={this.state.openDialogWrong} />
