9ae5905c95cea4274e401b4ddbaf88c98594f351
Raffi Dilanchian <raffidilanchian@gmail.com>
2017-03-11 14:32:21 +0330

    AnswerCard number

--- src/SP1/index.js
+++ src/SP1/index.js
@@ -3,6 +3,7 @@ import {
   IconButton,
   Grid,
   Cell,
+  Icon,
 } from 'react-mdl';
 import { shuffle } from 'underscore';
 import Layout from '../../components/Layout';
@@ -71,10 +72,10 @@ export default class Question extends React.Component {
 
         </Grid>
         <Grid style={{ fontSize: '30px'  }} className="demo-grid-1">
-          <Cell offset={1} col={1} align="center">1</Cell>
-          <Cell offset={2} col={1}>2</Cell>
-          <Cell offset={2} col={1}>3</Cell>
-          <Cell offset={2} col={1}>4</Cell>
+          <Cell offset={1} col={1} align="center"><Icon style={{ color: '#283593', fontSize: '50px' }} name="looks_one" /></Cell>
+          <Cell offset={2} col={1} ><Icon style={{ color: '#283593', fontSize: '50px' }} name="looks_two" /></Cell>
+          <Cell offset={2} col={1}><Icon style={{ color: '#283593', fontSize: '50px' }} name="looks_3" /></Cell>
+          <Cell offset={2} col={1}><Icon style={{ color: '#283593', fontSize: '50px' }} name="looks_4" /></Cell>
         </Grid>
         <div style={{ marginLeft: '299px', marginTop: '15px' }}> <Timer timeout={20} /></div>
         <ResultDialog score="5" correct={this.state.openDialogCorrect} wrong={this.state.openDialogWrong} />
